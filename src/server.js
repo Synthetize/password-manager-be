0d29125386ab8e32dad5e4b4a8e6785da13eb515
Leonardo Salpini <leonardo.salpini00@gmail.com>
2023-03-30 00:34:36 +0200

    Split file server.js

---
+++ src/server.js
@@ -0,0 +1,49 @@
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const {register, login} = require("./login-register");
+const {connect} = require("./db_connect-close");
+
+
+
+
+
+
+//app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:8080',
+}));
+app.set('view engine', 'ejs');
+con().then()
+async function con() {
+    await connect();
+}
+
+//-----------------Home-----------------
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
+
+//-----------------Login-----------------
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/api/login', async (req, res) => {
+    await login(req, res);
+});
+//-----------------Register-----------------
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/api/register', async (req, res) => {
+    await register(req, res);
+});
+//-----------------Port-----------------
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
